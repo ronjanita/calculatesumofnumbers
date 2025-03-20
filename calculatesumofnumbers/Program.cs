b6323f005e64663d43a8b97f37a8a1d51221363d
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-03-20 14:00:44 +0100

    uplaoded file with code

---
+++ calculatesumofnumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("enter number:");
+        int input = Convert.ToInt32(Console.ReadLine());
+        int resultat = 0;
+        for (int i = 0; i < input; i++) ;
+        {
+            int i = 0;
+            resultat += i;
+        }
+        Console.WriteLine($"resultat: {resultat}");
+    }
+}
